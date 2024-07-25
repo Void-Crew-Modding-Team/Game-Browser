9372052ecc1be5b8b934a9f57293cf3e8d251534
TheRealMesteven <mestevenyoutubing@gmail.com>
2024-07-25 17:52:22 +0100

    Added all columns

--- Game Browser/GameBrowserUI.cs
+++ Game Browser/GameBrowserUI.cs
@@ -57,9 +57,13 @@ namespace Game_Browser
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
-            if (GUILayout.Button("Room Name", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.RoomName);
-            if (GUILayout.Button("Current Players", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.CurrentPlayers);
-            if (GUILayout.Button("Max Players", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.MaxPlayers);
+            if (GUILayout.Button("Game Name", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.RoomName);
+            if (GUILayout.Button("Players", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.Players);
+            if (GUILayout.Button("Avg Rank", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.AverageRank);
+            if (GUILayout.Button("Difficulty", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.QuestDifficulty);
+            if (GUILayout.Button("System", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.SystemName);
+            if (GUILayout.Button("Ship", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.ShipName);
+            if (GUILayout.Button("Status", GUILayout.ExpandWidth(true))) SortByColumn(SortColumn.InHub);
             GUILayout.EndHorizontal();
             roomList.Sort(new RoomComparer { SortBy = (SortColumn)Config.currentSortColumn.Value, Ascending = ascending });
 
@@ -73,11 +77,15 @@ namespace Game_Browser
                 {
                     selectedRoom = roomInfo;
                 }
-
+                float AverageWidth = buttonRect.width / 7;
                 GUILayout.BeginHorizontal();
-                GUI.Label(new Rect(buttonRect.x, buttonRect.y, buttonRect.width / 3, buttonRect.height), $"     {roomInfo.RoomName}");
-                GUI.Label(new Rect(buttonRect.x + buttonRect.width / 3, buttonRect.y, buttonRect.width / 3, buttonRect.height), $"     {roomInfo.CurrentPlayers} / {roomInfo.MaxPlayers}");
-                GUI.Label(new Rect(buttonRect.x + 2 * buttonRect.width / 3, buttonRect.y, buttonRect.width / 3, buttonRect.height), $"     {roomInfo.MaxPlayers.ToString()}");
+                GUI.Label(new Rect(buttonRect.x, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.RoomName}");
+                GUI.Label(new Rect(buttonRect.x + AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.CurrentPlayers} / {roomInfo.MaxPlayers}");
+                GUI.Label(new Rect(buttonRect.x + 2 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.AverageRank}");
+                GUI.Label(new Rect(buttonRect.x + 3 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.QuestDifficulty}");
+                GUI.Label(new Rect(buttonRect.x + 4 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.SystemName}");
+                GUI.Label(new Rect(buttonRect.x + 5 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.ShipName}");
+                GUI.Label(new Rect(buttonRect.x + 6 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{(roomInfo.InHub ? "In Hub" : "Quest")}");
                 GUILayout.EndHorizontal();
             }
             GUI.DragWindow();
@@ -97,8 +105,12 @@ namespace Game_Browser
         private enum SortColumn
         {
             RoomName,
-            CurrentPlayers,
-            MaxPlayers
+            Players,
+            AverageRank,
+            QuestDifficulty,
+            SystemName,
+            ShipName,
+            InHub
         }
         private class RoomComparer : IComparer<MatchmakingRoom>
         {
@@ -113,11 +125,23 @@ namespace Game_Browser
                     case SortColumn.RoomName:
                         result = string.Compare(x.RoomName, y.RoomName);
                         break;
-                    case SortColumn.CurrentPlayers:
+                    case SortColumn.Players:
                         result = x.CurrentPlayers.CompareTo(y.CurrentPlayers);
                         break;
-                    case SortColumn.MaxPlayers:
-                        result = x.MaxPlayers.CompareTo(y.MaxPlayers);
+                    case SortColumn.AverageRank:
+                        result = x.AverageRank.CompareTo(y.AverageRank);
+                        break;
+                    case SortColumn.QuestDifficulty:
+                        result = x.QuestDifficulty.CompareTo(y.QuestDifficulty);
+                        break;
+                    case SortColumn.SystemName:
+                        result = x.SystemName.CompareTo(y.SystemName);
+                        break;
+                    case SortColumn.ShipName:
+                        result = x.ShipName.CompareTo(y.ShipName);
+                        break;
+                    case SortColumn.InHub:
+                        result = x.InHub.CompareTo(y.InHub);
                         break;
                 }
                 return Ascending ? result : -result;
