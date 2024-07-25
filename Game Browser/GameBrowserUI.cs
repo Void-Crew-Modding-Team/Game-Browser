ab38b10c7b5aeb5119ca6d93553ac917b4b20689
TheRealMesteven <mestevenyoutubing@gmail.com>
2024-07-25 16:41:01 +0100

    Made Menu Retrieve Rooms

--- Game Browser/GameBrowserUI.cs
+++ Game Browser/GameBrowserUI.cs
@@ -14,19 +14,32 @@ namespace Game_Browser
         #region Matchmaking Menu
         private void OnGUI()
         {
-            if (GameStateMachine.Instance?.CurrentState is GSMainMenu)
-            {
+            /*if (GameStateMachine.Instance?.CurrentState is GSMainMenu)
+            {*/
                 if (GUI.Button(new Rect(10f, 47.5f, 30f, 60f), "M") && Time.time - updatetime != 1f)
                 {
                     updatetime = Time.time;
                     guiActive = !guiActive;
                 }
-            }
-            else guiActive = false;
+            /*}
+            else guiActive = false;*/
             if (guiActive)
             {
                 WindowPos = new Rect(Screen.width / 4f, Screen.height / 4f, Screen.width / 2f, Screen.height / 2f);
                 GUI.Window(14290, WindowPos, new GUI.WindowFunction(WindowFunction), "Game Browser");
+            }
+        }
+        private void Update()
+        {
+            if (guiActive && !retrievingRooms)
+            {
+                MatchmakingHandler.Instance.StartRetrievingRooms();
+                retrievingRooms = true;
+            }
+            else if (!guiActive && retrievingRooms)
+            {
+                MatchmakingHandler.Instance.StopRetrievingRooms();
+                retrievingRooms = false;
             }
         }
         private void WindowFunction(int WindowID)
@@ -36,17 +49,25 @@ namespace Game_Browser
             GUILayout.BeginHorizontal();
             GUITools.DrawCheckbox("Show Full Rooms", ref Config.showFullRooms);
             GUITools.DrawCheckbox("Show Empty Rooms", ref Config.showEmptyRooms);
+            GUILayout.Label($"Retrieving Rooms: {retrievingRooms}");
             GUILayout.EndHorizontal();
             List<MatchmakingRoom> roomList = MatchmakingHandler.Instance.GetRooms(Config.showFullRooms.Value, Config.showEmptyRooms.Value);
             GUILayout.Label($"Found {roomList.Count} rooms");
             foreach (MatchmakingRoom roomInfo in roomList)
             {
+                if (roomInfo == null) return;
                 if (GUILayout.Button($"{roomInfo.RoomName} | {roomInfo.CurrentPlayers} / {roomInfo.MaxPlayers}")) selectedRoom = roomInfo;
             }
         }
         private MatchmakingRoom selectedRoom;
-        private bool guiActive = false;
+        private static bool guiActive = false;
+        public static bool GuiActive
+        {
+            get { return guiActive; }
+            private set { guiActive = value; }
+        }
         private float updatetime;
+        private bool retrievingRooms = false;
         private Rect WindowPos;
         #endregion
         #region Matchmaking Methods
