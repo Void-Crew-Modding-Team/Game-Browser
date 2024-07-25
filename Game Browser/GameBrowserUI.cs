82a9b5ea091cbb94ee9e498f571de3ef1ebc0c0d
Mest <mestevenyoutubing@gmail.com>
2024-07-25 16:44:51 +0100

    Created functional room list (#1)
    * Removed unnecessary comments
    
    * Created RoomList Menu
    
    * Made Menu Retrieve Rooms

--- Game Browser/GameBrowserUI.cs
+++ Game Browser/GameBrowserUI.cs
@@ -1,58 +1,77 @@
 ﻿using CG.GameLoopStateMachine;
 using CG.GameLoopStateMachine.GameStates;
 using CG.Input;
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UI.Matchmaking;
 using UnityEngine;
+using VoidManager.Utilities;
 
 namespace Game_Browser
 {
-    internal class GameBrowserUI : MonoBehaviour, IShowCursorSource, IInputActionMapRequest
+    internal class GameBrowserUI : MatchmakingHandler
     {
-        /*private void Update()
-        {
-            bool flag = PauseManager.IsPaused != this.guiActive;
-            if (flag)
-            {
-                InputActionMapRequests.RemoveRequest(this);
-                this.guiActive = PauseManager.IsPaused;
-                bool flag2 = this.guiActive;
-                if (flag2)
-                {
-                    this.WindowPos = new Rect((float)Screen.width / 4f, (float)Screen.height / 4f, (float)Screen.width / 2f, (float)Screen.height / 2f);
-                }
-                CursorUtility.ShowCursor(this, this.guiActive);
-            }
-        }*/
+        #region Matchmaking Menu
         private void OnGUI()
         {
-            if (GameStateMachine.Instance?.CurrentState is GSMainMenu)
-            {
+            /*if (GameStateMachine.Instance?.CurrentState is GSMainMenu)
+            {*/
                 if (GUI.Button(new Rect(10f, 47.5f, 30f, 60f), "M") && Time.time - updatetime != 1f)
                 {
                     updatetime = Time.time;
                     guiActive = !guiActive;
                 }
-            }
-            else guiActive = false;
+            /*}
+            else guiActive = false;*/
             if (guiActive)
             {
-                //InputActionMapRequests.AddOrChangeRequest(this, InputStateRequestType.UI);
                 WindowPos = new Rect(Screen.width / 4f, Screen.height / 4f, Screen.width / 2f, Screen.height / 2f);
                 GUI.Window(14290, WindowPos, new GUI.WindowFunction(WindowFunction), "Game Browser");
             }
         }
+        private void Update()
+        {
+            if (guiActive && !retrievingRooms)
+            {
+                MatchmakingHandler.Instance.StartRetrievingRooms();
+                retrievingRooms = true;
+            }
+            else if (!guiActive && retrievingRooms)
+            {
+                MatchmakingHandler.Instance.StopRetrievingRooms();
+                retrievingRooms = false;
+            }
+        }
         private void WindowFunction(int WindowID)
         {
-            GUILayout.Label("Hello!");
             if (GUILayout.Button("Close")) guiActive = false;
-
+            if (MatchmakingHandler.Instance == null) return;
+            GUILayout.BeginHorizontal();
+            GUITools.DrawCheckbox("Show Full Rooms", ref Config.showFullRooms);
+            GUITools.DrawCheckbox("Show Empty Rooms", ref Config.showEmptyRooms);
+            GUILayout.Label($"Retrieving Rooms: {retrievingRooms}");
+            GUILayout.EndHorizontal();
+            List<MatchmakingRoom> roomList = MatchmakingHandler.Instance.GetRooms(Config.showFullRooms.Value, Config.showEmptyRooms.Value);
+            GUILayout.Label($"Found {roomList.Count} rooms");
+            foreach (MatchmakingRoom roomInfo in roomList)
+            {
+                if (roomInfo == null) return;
+                if (GUILayout.Button($"{roomInfo.RoomName} | {roomInfo.CurrentPlayers} / {roomInfo.MaxPlayers}")) selectedRoom = roomInfo;
+            }
         }
-        private bool guiActive = false;
+        private MatchmakingRoom selectedRoom;
+        private static bool guiActive = false;
+        public static bool GuiActive
+        {
+            get { return guiActive; }
+            private set { guiActive = value; }
+        }
         private float updatetime;
+        private bool retrievingRooms = false;
         private Rect WindowPos;
+        #endregion
+        #region Matchmaking Methods
+
+        #endregion
     }
 }
---
+++ Game Browser/Patches.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Browser
+{
+    [HarmonyPatch(typeof(MatchmakingHandler), "StopRetrievingRooms")]
+    internal class StopRetrievingRoomsPatch
+    {
+        private static bool Prefix()
+        {
+            if (GameBrowserUI.GuiActive) return false;
+            return true;
+        }
+    }
+}
