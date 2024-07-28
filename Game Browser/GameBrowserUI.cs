ed2055047fc9b1d4b507233e8142a2556ba3aee3
Mest <mestevenyoutubing@gmail.com>
2024-07-28 15:56:30 +0100

    Improved Room List Menu (#3)
    * Can join games
    
    * Added checks to ensure menu is open only on BASE menu screens
    
    * Added Create Hub Option
    
    * Added Room Details
    
    * Added info for when Room Retrieval Fails
    
    * Added exception protection for changing region
    
    * Removed exception protection (It didnt work)

--- Game Browser/GameBrowserUI.cs
+++ Game Browser/GameBrowserUI.cs
@@ -1,53 +1,81 @@
-﻿using CG.GameLoopStateMachine;
-using CG.GameLoopStateMachine.GameStates;
-using CG.Input;
-using System.Collections;
+﻿using CG.GameLoopStateMachine.GameStates;
+using CG.GameLoopStateMachine;
+using System;
 using System.Collections.Generic;
+using ToolClasses;
 using UnityEngine;
 using VoidManager.Utilities;
+using HarmonyLib;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Photon.Pun;
+using System.Threading.Tasks;
+using Photon.Realtime;
 
 namespace Game_Browser
 {
     internal class GameBrowserUI : MatchmakingHandler
     {
         #region Matchmaking Menu
+        private void Start()
+        {
+            WindowPos = new Rect(Screen.width / 4f, Screen.height / 4f, Screen.width / 2f, Screen.height / 2f);
+        }
         private void OnGUI()
         {
-            /*if (GameStateMachine.Instance?.CurrentState is GSMainMenu)
-            {*/
+            if (MenuScreenController.Instance?.OpenCount == 1)
+            {
                 if (GUI.Button(new Rect(10f, 47.5f, 30f, 60f), "M") && Time.time - updatetime != 1f)
                 {
                     updatetime = Time.time;
                     guiActive = !guiActive;
                 }
-            /*}
-            else guiActive = false;*/
+            }
+            else guiActive = false;
             if (guiActive)
             {
-                WindowPos = new Rect(Screen.width / 4f, Screen.height / 4f, Screen.width / 2f, Screen.height / 2f);
                 GUI.Window(14290, WindowPos, new GUI.WindowFunction(WindowFunction), "Game Browser");
             }
         }
+        private static FieldInfo InstanceInfo = AccessTools.Field(typeof(MatchmakingHandler), "_instance");
+        private static FieldInfo stateInfo = AccessTools.Field(typeof(MatchmakingHandler), "state");
+        private static FieldInfo roomsInfo = AccessTools.Field(typeof(MatchmakingHandler), "rooms");
+        /*
+        public enum RoomFetchState
+	    {
+		    Disconnected,
+		    ConnectingToMasterServer,
+		    ConnectingToLobby,
+		    UpdatingRooms
+	    }
+        */
         private void Update()
         {
-            if (guiActive && !retrievingRooms)
+            if (guiActive && !retrievingRooms) //OnEnable
             {
                 MatchmakingHandler.Instance.StartRetrievingRooms();
                 retrievingRooms = true;
             }
-            else if (!guiActive && retrievingRooms)
+            else if (!guiActive && retrievingRooms) //OnDisable()
             {
                 MatchmakingHandler.Instance.StopRetrievingRooms();
                 retrievingRooms = false;
             }
+            if (MatchmakingHandler.Instance == null) InstanceInfo.SetValue(this, this);
         }
         private void WindowFunction(int WindowID)
         {
             if (GUILayout.Button("Close")) guiActive = false;
-            if (MatchmakingHandler.Instance == null) return;
+            if (MatchmakingHandler.Instance == null || PhotonService.Instance?.CurrentRegion() == null) return;
 
             List<MatchmakingRoom> roomList = MatchmakingHandler.Instance.GetRooms(Config.showFullRooms.Value, Config.showEmptyRooms.Value);
+            if (!roomList.Contains(selectedRoom)) selectedRoom = null;
+            GUILayout.BeginHorizontal();
+            string pattern = @"\[.*?\]:";
+            string region = Regex.Replace(PhotonService.Instance?.CurrentRegion()?.ToString(), pattern, string.Empty);
+            GUILayout.Label($"Current Region: {region}");
             GUILayout.Label($"Found {roomList.Count} rooms");
+            GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
             GUITools.DrawCheckbox("Show Full Rooms", ref Config.showFullRooms);
@@ -68,27 +96,44 @@ namespace Game_Browser
 
 
             GUILayout.Space(10);
-            foreach (MatchmakingRoom roomInfo in roomList)
+            if (roomList.Count != 0)
             {
-                if (roomInfo == null) return;
-                Rect buttonRect = GUILayoutUtility.GetRect(GUIContent.none, GUIStyle.none, GUILayout.ExpandWidth(true), GUILayout.Height(30));
-                if (GUI.Button(buttonRect, GUIContent.none))
+                GUILayout.BeginScrollView(scrollPosition);
+                foreach (MatchmakingRoom roomInfo in roomList)
                 {
-                    selectedRoom = roomInfo;
+                    if (roomInfo == null) return;
+                    Rect buttonRect = GUILayoutUtility.GetRect(GUIContent.none, GUIStyle.none, GUILayout.ExpandWidth(true), GUILayout.Height(35));
+                    if (GUI.Button(buttonRect, GUIContent.none))
+                    {
+                        selectedRoom = roomInfo;
+                    }
+                    FormattedRect(buttonRect, new List<string>() { roomInfo.RoomName, $"{roomInfo.CurrentPlayers} / {roomInfo.MaxPlayers}", $"{roomInfo.AverageRank}", roomInfo.QuestDifficulty, roomInfo.SystemName, roomInfo.ShipName, $"{(roomInfo.InHub ? "In Hub" : "Quest")}" });
                 }
-                float AverageWidth = buttonRect.width / 7;
-                GUILayout.BeginHorizontal();
-                GUI.Label(new Rect(buttonRect.x, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.RoomName}");
-                GUI.Label(new Rect(buttonRect.x + AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.CurrentPlayers} / {roomInfo.MaxPlayers}");
-                GUI.Label(new Rect(buttonRect.x + 2 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.AverageRank}");
-                GUI.Label(new Rect(buttonRect.x + 3 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.QuestDifficulty}");
-                GUI.Label(new Rect(buttonRect.x + 4 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.SystemName}");
-                GUI.Label(new Rect(buttonRect.x + 5 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{roomInfo.ShipName}");
-                GUI.Label(new Rect(buttonRect.x + 6 * AverageWidth, buttonRect.y, AverageWidth, buttonRect.height), $"{(roomInfo.InHub ? "In Hub" : "Quest")}");
-                GUILayout.EndHorizontal();
+                GUILayout.EndScrollView();
+
+                if (selectedRoom != null)
+                {
+                    GUILayout.Label("<b>Room Info</b>");
+                    Rect buttonRect2 = GUILayoutUtility.GetRect(GUIContent.none, GUIStyle.none, GUILayout.ExpandWidth(true), GUILayout.Height(35));
+                    FormattedRect(buttonRect2, new List<string>() { selectedRoom.RoomName, $"{selectedRoom.CurrentPlayers} / {selectedRoom.MaxPlayers}", $"{selectedRoom.AverageRank}", selectedRoom.QuestDifficulty, selectedRoom.SystemName, selectedRoom.ShipName, $"{(selectedRoom.InHub ? "In Hub" : "Quest")}" });
+                    if (GUILayout.Button("Join Game"))
+                    {
+                        JoinRequested();
+                    }
+                }
             }
-            GUI.DragWindow();
+            else
+            {
+                GUILayout.BeginScrollView(scrollPosition);
+                GUILayout.Label($"{(retrievingRooms ? "Searching for rooms . . ." : "Unable to retrieve rooms" )}");
+                GUILayout.EndScrollView();
+            }
+            if (GUILayout.Button("Create Hub"))
+            {
+                JoinHub();
+            }
         }
+        private Vector2 scrollPosition;
         private MatchmakingRoom selectedRoom;
         private static bool guiActive = false;
         public static bool GuiActive
@@ -100,7 +145,67 @@ namespace Game_Browser
         private bool retrievingRooms = false;
         private Rect WindowPos;
         #endregion
-        #region Matchmaking Methods
+        #region Matchmaking Menu Methods
+        private void JoinRequested()
+        {
+            if (selectedRoom == null)
+            {
+                //this.failPopup.Show(AbstractDataTable<UIHelperData>.Instance.RoomDoesNotExist.String);
+                return;
+            }
+            MatchmakingHandler.RoomJoinStatus roomJoinStatus = MatchmakingHandler.Instance.JoinGame(selectedRoom.RoomId);
+            switch (roomJoinStatus)
+            {
+                case MatchmakingHandler.RoomJoinStatus.RoomFull:
+                    //this.failPopup.Show(AbstractDataTable<UIHelperData>.Instance.RoomFull.String);
+                    return;
+                case MatchmakingHandler.RoomJoinStatus.RoomDoesNotExist:
+                    //this.failPopup.Show(AbstractDataTable<UIHelperData>.Instance.RoomDoesNotExist.String);
+                    return;
+                case MatchmakingHandler.RoomJoinStatus.RoomIsPrivate:
+                    //this.failPopup.Show(AbstractDataTable<UIHelperData>.Instance.RoomPrivate.String);
+                    return;
+                default:
+                    return;
+            }
+        }
+        private async void JoinHub()
+        {
+            if (PhotonNetwork.CurrentRoom != null && PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("R_IH"))
+            {
+                Singleton<GameStateMachine>.Instance.ChangeState<GSQuitFromMenu>();
+                Singleton<MenuControllerUGUI>.I.CloseTopMenu();
+
+                // Wait asynchronously until the PhotonNetwork.CurrentRoom is null
+                await WaitForRoomToBeNullAsync();
+            }
+            Singleton<SteamService>.I.CreateLobby().Then(() => PunSingleton<PhotonService>.I.CreateRoom(false)).Then(new Action(this.LaunchHub));
+        }
+        private async Task WaitForRoomToBeNullAsync()
+        {
+            while (!(Singleton<GameStateMachine>.Instance.CurrentState is GSMainMenu))
+            {
+                await Task.Delay(100); // Check every 100ms
+            }
+        }
+        private void LaunchHub()
+        {
+            GameSessionManager.Instance.SetNextGameSession(GameSessionLoadPromise.LoadLobbyData());
+            Singleton<GameStateMachine>.Instance.ChangeState<GSLoadingGame>();
+        }
+        #endregion
+        #region Matchmaking Menu Formatting
+
+        private void FormattedRect(Rect buttonRect, List<string> strings)
+        {
+            float AverageWidth = buttonRect.width / strings.Count;
+            GUILayout.BeginHorizontal();
+            for (int i = 0; i < strings.Count; i++)
+            {
+                GUI.Label(new Rect(buttonRect.x + (i * AverageWidth), buttonRect.y, AverageWidth, buttonRect.height), $"{strings[i]}");
+            }
+            GUILayout.EndHorizontal();
+        }
 
         private enum SortColumn
         {
